a02b79b78203163260c14a67834b9b485616e7bf
Paul Flores <paulfloresdev@icloud.com>
2024-12-02 23:16:31 -0600

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
 
-import robotsTxt from "astro-robots-txt"
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  base: '/',
-})
+  integrations: [tailwind(), robotsTxt(), react()],
+  base: '/'
+});
