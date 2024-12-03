fbbd480d569709945858cd295f4d53497054254b
Paul Flores <paulfloresdev@icloud.com>
2024-12-02 23:16:46 -0600

    first commit

---
+++ src/pages/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function HelloWorld() {
+    return (
+        <div className='justify-items-center text-center'>
+            <h1>Hello, World!</h1>
+            <a href="/porfolio" className='items-center'>
+                <button>Porfolio</button>
+            </a>
+        </div>
+    );
+}
+
+export default HelloWorld;
