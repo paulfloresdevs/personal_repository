ae00c2a487547146b41313ad4c2668dcdad2dfe8
Paul Flores <paulfloresdev@icloud.com>
2024-12-02 23:50:08 -0600

    add an alias at configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import robotsTxt from 'astro-robots-txt';
-import react from '@astrojs/react';
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+import react from "@astrojs/react";
+import path from 'path';
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt(), react()],
-  base: '/'
+  base: '/',
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src')
+      }
+    }
+  }
 });
