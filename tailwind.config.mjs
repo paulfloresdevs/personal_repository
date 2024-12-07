484204c3dac096bac0845640c4adbf5e43d6ca1c
Paul Flores <paulfloresdev@icloud.com>
2024-12-06 21:21:20 -0600

    Delete unnecesary plugin

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,8 +4,16 @@ export default {
 	darkMode: 'class',
 	theme: {
 		extend: {
+			fontFamily: {
+				space: ['Space Mono', 'monospace'],
+			},
+
 			height: {
 			  'custom': '52px', // Ajusta este valor según lo que consideres un tamaño medio (13 * 4px = 52px)
+			},
+
+			"ion-icon": {
+				'font-size': '64px',
 			}
 		}
 	},
