cd9b5cfce5efea86e21c674a78e56609b4c6860c
Paul Flores <paulfloresdev@icloud.com>
2024-12-06 21:20:21 -0600

    add Navbar and principal components

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import Navbar from '@/components/main/navbar';
+import Principal from '@/components/main/principal-page';
 
 function HelloWorld() {
     return (
         <div className='justify-items-center text-center'>
-            <h1>Hello, World!</h1>
-            <a href="/porfolio" className='items-center'>
-                <button>Porfolio</button>
-            </a>
+            <Navbar />
+            <Principal />
         </div>
     );
 }
